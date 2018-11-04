072bca056811ee82589110ad1ffe7f0e14b96cd6
Felipe Apablaza <ing.apablaza@gmail.com>
2018-11-03 21:53:59 -0300

    Implementacion inicial de la carga de cartolas

--- app/batch/import-movements.js
+++ app/batch/import-movements.js
@@ -3,5 +3,46 @@
  */
 
 const scotiabank = require('../util/scotiabank');
-const models = require('./db/models');
+const models = require('../db/models');
+const fs = require('fs');
 
+const files = [
+    'data/scotiabank/bns (0).csv',
+    'data/scotiabank/bns (1).csv',
+    'data/scotiabank/bns (2).csv',
+    'data/scotiabank/bns (3).csv',
+    'data/scotiabank/bns (4).csv'
+];
+
+const movements = files
+  .map((file) => {
+    return fs.readFileSync(file).toString().split('\n');
+  })
+  .reduce((previous, current) => {
+    console.log(previous);
+    const result = [];
+
+    result.concat(previous);
+    result.concat(current);
+
+    return result;
+  });
+
+scotiabank.importBankStatement(movements).then((statement) => {
+  console.log(statement);
+}).catch((err) => {
+  console.log(err);
+});
+
+/*
+fs.readFile('./bsa.dat', (err, data) => {
+  const lines = data.toString().split('\n');
+
+  importPayroll(lines).then((nomina) => {
+    console.log(nomina.detalles);
+    // console.log(nomina.detalles.filter(detalle => detalle.estado !== 'Pagado'));
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+*/
