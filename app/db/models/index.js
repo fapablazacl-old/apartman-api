23e9c32f9555f08dabbef83b443416117fbe3f4b
Felipe Apablaza <ing.apablaza@gmail.com>
2018-11-04 22:08:34 -0300

    Agregadas credenciales de conexion

--- app/db/models/index.js
+++ app/db/models/index.js
@@ -2,10 +2,10 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize (
-  'apartman', 'postgres', 'postgres', {
-    // host: '172.18.0.2',
-    host: 'localhost',
-    dialect: 'postgres'
+  'd8g9hahqsm6sht', 'mxiuygymilkeom', '936db37e6c9b66a75843c17fc7630149a1177910ab26ff3e586a3bdc30d53f19', {
+    host: 'ec2-54-225-115-234.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: 5432,
   }
 );
 
