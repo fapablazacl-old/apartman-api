62f3cd609b718047386f4e7fcb9000c4ffdb191b
Felipe Apablaza <ing.apablaza@gmail.com>
2018-11-04 21:54:59 -0300

    Agregado codigo de importacion de nominas

---
+++ app/batch/import-payrolls.js
@@ -0,0 +1,45 @@
+/**
+ * Importa las cartolas historicas del banco Scotiabank desde archivos CSV.
+ */
+
+const scotiabank = require('../util/scotiabank');
+const models = require('../db/models');
+const fs = require('fs');
+
+const files = [
+    'data/scotiabank/bns (0).csv',
+    'data/scotiabank/bns (1).csv',
+    'data/scotiabank/bns (2).csv',
+    'data/scotiabank/bns (3).csv',
+    'data/scotiabank/bns (4).csv'
+];
+
+Promise.all(files.map((file) => {
+  const lines = fs.readFileSync(file).toString().split('\n');
+  return scotiabank.importPayroll(lines);
+})).then((payrolls) => {
+
+    
+
+
+  const movements = statements.reduce((previous, current) => {
+    return previous.concat(current);
+  });
+
+  Promise.all(movements.map((movement) => {
+    return models.Movements.create({
+      bank: 'SB',
+      date: movement.fecha,
+      amount: movement.cargos != null ? -movement.cargos : movement.abonos,
+      description: movement.descripcion,
+      documentNumber: movement.ndoc,
+    });
+  })).then((results) => {
+    console.log('Done!');
+    console.log(results);
+  }).catch((err) => {
+    console.log(err);
+  });
+}).catch((err) => {
+  console.log(err);
+});
